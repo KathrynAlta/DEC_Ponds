ff129fa153c843d6ee17718898236176459cbdbc
Kathryn A Gannon <kag326@cornell.edu>
2023-01-09 13:05:59 -0500

    Figuring out geocomp in R

---
+++ Geocomputation_with_R/GCwR_Ch2_KG.R
@@ -0,0 +1,87 @@
+# Geocomputation with R 
+# Chapter 2 Notes 
+
+ #__________________________________________________________________________________________________
+# 0. Set up R Environment 
+
+    # Set working directory 
+      setwd("~/Sediment_Mapping_DEC") # Desktop 
+      getwd()
+    
+    # Load standard packages 
+      library(dplyr)
+      library(raster)
+      library(ape) 
+      library(mosaic)
+      library(readxl)
+      library(tidyverse)
+      library(devtools)
+      library(lme4)
+      library(car)
+      library(effects)
+      library(mosaic)
+      library(writexl)
+      library(purrr)
+      library(EnvStats)
+    
+    # Spatial packages 
+      library(sf)   # classes and functions for vector data
+      library(ggspatial)
+      library(terra)   # classes and functions for raster data
+      library(spData) # load geographic data
+      library(spDataLarge)  # load larger geographic data
+    
+    
+    # Install software packages 
+      install.packages("terra")
+      install.packages("spData")
+      install.packages("spDataLarge", repos = "https://nowosad.r-universe.dev")
+      remotes::install_github("geocompr/geocompkg")
+      remotes::install_github("geocompr/geocompkg", dependencies = TRUE)
+
+#__________________________________________________________________________________________________
+# 2.2.1 An Introduction to Simple Features 
+    vignette(package = "sf") # see which vignettes are available
+    vignette("sf1")          # an introduction to the package
+    
+    # Exploring the world 
+    class(world)
+    head(world)
+    names(world)
+    world$geom
+    world_df <- as.data.frame(world)
+    plot(world)
+    summary(world["lifeExp"])
+    world_mini <- world[1:3, 1:2]
+    head(world_mini)
+    world_mini
+    
+# 2.2.2 Why Simple Features?    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
--- Learning_Bathymetry_Code.R
+++ Learning_Bathymetry_Code.R
@@ -1,10 +1,33 @@
 ############################
 # BATHYMETRY & LAKE VOLUME ESTIMATION USING R
 ############################
+
+# RESOURCES:  
+# ______________________________________
 # Tutorial : https://dewey.dunnington.ca/post/2019/bathymetry-lake-volume-estimation-using-r/ 
 # GIS Course: https://mgimond.github.io/Spatial/spatial-interpolation.html 
 # Once you are set up with GIS through cornell you should have access to the online Esri trainings: https://www.esri.com/training/unlimited-esri-training/ 
       # Free Esri Intro course: https://www.esri.com/training/catalog/5c9a7395190cf23eac62a998/getting-started-with-data-management/ 
+# Mapping in R : https://geocompr.robinlovelace.net/adv-map.html 
+# Git https://github.com/geocompx/geocompr 
+# Book about Geospatial: https://geocompr.robinlovelace.net/intro.html #I am liking working through this one -KG 010923
+
+
+# NOTES : 
+# ______________________________________
+# Steps in calculating volume of sediment:  
+#     1. Set the boundary for the pond as a polygons [pond_poly]
+#           --> need to make in ArcGIS 
+#     2. Turn the depth data (collected by sediment mapping in the field) into a shape file [sed_depths]
+#           --> should be able to just open the csv of this (with lat long) in ArcGIS and then save it as a shapefile
+#     3. Put together the pond_poly and sed_depths 
+#     4. use the data that you have to interpolate more data points (depth measurements) in the pond (fine grain bathymetry)
+#           --> ause some sort of interpolation math (ex: TIN, IDW, TPRS etc) 
+#     5. use all of those points (measured + interpolated) to estimate the volume of sediment 
+#         (mean sediment depth) * (area of the pond) = volume of sediment 
+
+
+
 
 #__________________________________________________________________________________________________
 # 0. Set up R Environment 
