5d0ad632548a1cc5f7dfac04864ffbb183ddde70
Kathryn A Gannon <kag326@cornell.edu>
2023-02-14 12:19:45 -0500

    trying to interpolate updateing packages

--- 03_Dunnington_Code_Mapping_Sediment_Depth.R
+++ 03_Dunnington_Code_Mapping_Sediment_Depth.R
@@ -30,6 +30,10 @@
     
     library(sf)
     library(ggspatial)
+    install.packages("Rtools")
+    install.packages("interpp")
+    install.packages("gstat")
+    library(interpp)
     
 # 1. Measured Depths 
     
@@ -83,13 +87,19 @@
      geom_sf_text(aes(label = depth), data = measured_depths, size = 2.5) +
      annotation_scale(location = "br")
    
-   # Example Data 
+   # Holgerson Data - Sediment Depth 
    ggplot() +
      geom_sf(data = pond_boundary) +
      geom_sf_text(aes(label = sed_depth), data = pond_depths, size = 2.5) +
      annotation_scale(location = "br")
     
-# 4. Add the coordinates as columns 
+   # Example Data 
+   ggplot() +
+     geom_sf(data = pond_boundary) +
+     geom_sf_text(aes(label = water_depth), data = pond_depths, size = 2.5) +
+     annotation_scale(location = "br")
+   
+# 4. Add the coordinates of the boundary as columns and set the depth at the boundary to zero 
    
    # Example Data 
    boundary_points <- st_cast(boundary, "POINT")
@@ -101,6 +111,7 @@
    # Holgerson Lab Data 
    pond_boundary
    pond_boundary_points <- st_cast(pond_boundary, "POINT")
+   pond_boundary_points
    # Format pond boundary points 
      pond_name <- pond_depths[[1, "pond"]]
      formatted_pond_boundary_points <- pond_boundary_points %>% transmute(source = "boundary", pond = pond_name, water_depth = 0, sed_depth = 0)
@@ -108,26 +119,69 @@
      formatted_pond_boundary_points
      
    # Bind together boundary and depth 
+     formatted_pond_boundary_points
+     pond_depths
    full_pond_depths <- rbind(formatted_pond_boundary_points, pond_depths) %>%
      cbind(., st_coordinates(.))
    full_pond_depths
    
 # 5. Create a grid to hold the raster output 
+ 
    
-   # --- > Start here next time, trouble with the grid
+   #Example Code but I don't like the pipes here 
+       # grid <- st_make_grid(depths, cellsize = c(10, 10), what = "centers") %>%
+        # st_as_sf() %>% 
+        # filter(st_contains(boundary, ., sparse = FALSE)) %>%  # This step is trying to cut off the grid so that it is only in the boundary of the polygon of the waterbody 
+        # cbind(., st_coordinates(.))
    
-   #Example Data 
-   depths
-   grid <- st_make_grid(depths, cellsize = c(10, 10), what = "centers") %>%
-     st_as_sf() %>% 
-     filter(st_contains(boundary, ., sparse = FALSE)) %>%
-     cbind(., st_coordinates(.))
+   # Do step by step rather than in pipes 
+   grid_step1 <- st_make_grid(depths, cellsize = c(10, 10), what = "centers")
+   grid_step2 <- st_as_sf(grid_step1)
+   grid_step3 <- st_contains(boundary, grid_step2, sparse = FALSE)
+   grid_step4 <- grid_step2[grid_step3 == "TRUE" , ]
+   grid_step5 <- cbind(grid_step4, st_coordinates(grid_step4))
+   example_grid <- grid_step5
+
    
    # Holgerson Data 
+   # Do step by step rather than in pipes 
+   pond_grid_step1 <- st_make_grid(full_pond_depths, cellsize = c(5, 5), what = "centers")   #I made a bigger grid here (well smaller squares, more points)
+   pond_grid_step2 <- st_as_sf(pond_grid_step1)
+   pond_grid_step3 <- st_contains(pond_boundary, pond_grid_step2, sparse = FALSE)
+   pond_grid_step4 <- pond_grid_step2[pond_grid_step3 == "TRUE" , ]
+   pond_grid_step5 <- cbind(pond_grid_step4, st_coordinates(pond_grid_step4))
+   pond_grid <- pond_grid_step5
     
+# 6. TIN --> required packages not availale for this version of R 
+   # triangular irregular network surface (TIN) connects points using a Delaunay triangulation (a network of triangles as round as
+   # possible) and approximates each triangle as a plane. It results in contours that are pointier than you may be hoping for, but 
+   # doesn’t predict any values higher or lower than you measured and is a good reality check on any other method as its main 
+   # assumption is that you bothered to take a depth measurement anywhere that it mattered.
+   
+   # Exampl Data 
+   
+   fit_TIN <- interp::interpp(
+     x = depths$X,
+     y = depths$Y,
+     z = depths$depth,
+     xo = grid$X,
+     yo = grid$Y,
+     duplicate = "strip"
+   )
+   
+   grid$TIN <- fit_TIN$z
     
-    
-    
+# 7. [inverse distance weighted interpolation]    
+   fit_gstat <- gstat::gstat(
+     formula = depth ~ 1,
+     data = as(depths, "Spatial"),
+     nmax = 10, nmin = 3,
+     set = list(idp = 0.5)
+   )
+   
+   grid$IDW <- predict(fit_gstat, newdata = as(grid, "Spatial")) %>%
+     st_as_sf() %>%
+     pull(1)
     
     
     
