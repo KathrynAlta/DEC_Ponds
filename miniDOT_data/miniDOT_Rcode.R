ef00ebc0efbb24fc0f6211595d9c5f5535b40332
Meredith <meredith.holgerson@gmail.com>
2023-11-20 22:40:12 -0500

    minidot code

---
+++ miniDOT_data/miniDOT_Rcode.R
@@ -0,0 +1,156 @@
+# DO Data for 2023 DEC Ponds: Intensive ####
+## Code started by MAH 20 Nov 2023 ####
+
+
+#load packages ####
+library(ggplot2)
+library(tidyverse)
+library(scales)
+
+
+# Load data ####
+do_edwards <- read.csv("miniDOT_data/RawData/Edwards_2023.csv", strip.white=T, na.strings="na")
+do_harrison <- read.csv("miniDOT_data/RawData/Harrison_2023.csv", strip.white=T, na.strings="na")
+do_howarth <- read.csv("miniDOT_data/RawData/Howarth_2023.csv", strip.white=T, na.strings="na")
+do_levine <- read.csv("miniDOT_data/RawData/Levine_2023.csv", strip.white=T, na.strings="na")
+do_mtpleasantse <- read.csv("miniDOT_data/RawData/MtPleasantSE_2023.csv", strip.white=T, na.strings="na")
+do_shelterbelt <- read.csv("miniDOT_data/RawData/Shelterbelt_2023.csv", strip.white=T, na.strings="na")
+do_white <- read.csv("miniDOT_data/RawData/White_2023.csv", strip.white=T, na.strings="na")
+#do_boyce <- read.csv("miniDOT_data/RawData/Boyce_2023", strip.white=T, na.strings="na")
+
+
+
+head(do_edwards)
+head(do_harrison)
+head(do_howarth)
+head(do_levine)
+head(do_mtpleasantse)
+head(do_shelterbelt)
+head(do_white)
+head(do_boyce)
+
+#set date-time format####
+do_edwards$time2 <- as.POSIXct(do_edwards$datetime, "%Y-%m-%d %H:%M", tz="America/new_york")
+do_harrison$time2 <- as.POSIXct(do_harrison$datetime, "%Y-%m-%d %H:%M", tz="America/new_york")
+do_howarth$time2 <- as.POSIXct(do_howarth$datetime, "%Y-%m-%d %H:%M", tz="America/new_york")
+do_levine$time2 <- as.POSIXct(do_levine$datetime, "%Y-%m-%d %H:%M", tz="America/new_york")
+do_mtpleasantse$time2 <- as.POSIXct(do_mtpleasantse$datetime, "%Y-%m-%d %H:%M", tz="America/new_york")
+do_shelterbelt$time2 <- as.POSIXct(do_shelterbelt$datetime, "%Y-%m-%d %H:%M", tz="America/new_york")
+do_white$time2 <- as.POSIXct(do_white$datetime, "%Y-%m-%d %H:%M", tz="America/new_york")
+do_boyce$time2 <- as.POSIXct(do_boyce$datetime, "%Y-%m-%d %H:%M", tz="America/new_york")
+
+
+
+## PLOTS ####
+
+##*Edwards ####
+head(do_edwards)
+
+
+plot_do_edwards <- do_edwards %>% 
+  ggplot(aes(x=time2, y=do_perc)) + 
+  geom_hline(yintercept=100, linetype='dotted', col = 'blue')+
+  geom_point(size=0.5) + #geom_path()+
+  scale_x_datetime(breaks=date_breaks(width="7 days"), 
+                   labels=date_format("%m/%d", tz="America/new_york"))+ theme_bw() +
+  theme(text = element_text(size=12), axis.text.x = element_text(angle=-40))+ 
+  xlab("Time") + ylab("DO percent") #+ ylim(c(90,160))
+plot_do_edwards 
+
+
+
+##*Harrison ####
+head(do_harrison)
+
+
+plot_do_harrison <- do_harrison %>% 
+  ggplot(aes(x=time2, y=do_perc)) + 
+  geom_hline(yintercept=100, linetype='dotted', col = 'blue')+
+  geom_point(size=0.5) + #geom_path()+
+  scale_x_datetime(breaks=date_breaks(width="7 days"), 
+                   labels=date_format("%m/%d", tz="America/new_york"))+ theme_bw() +
+  theme(text = element_text(size=12), axis.text.x = element_text(angle=-40))+ 
+  xlab("Time") + ylab("DO percent") #+ ylim(c(90,160))
+plot_do_harrison
+
+
+##*Howarth ####
+head(do_howarth)
+
+
+plot_do_howarth <- do_howarth %>% 
+  ggplot(aes(x=time2, y=do_perc)) + 
+  geom_hline(yintercept=100, linetype='dotted', col = 'blue')+
+  geom_point(size=0.5) + #geom_path()+
+  scale_x_datetime(breaks=date_breaks(width="7 days"), 
+                   labels=date_format("%m/%d", tz="America/new_york"))+ theme_bw() +
+  theme(text = element_text(size=12), axis.text.x = element_text(angle=-40))+ 
+  xlab("Time") + ylab("DO percent") #+ ylim(c(90,160))
+plot_do_howarth 
+
+
+
+##*Levine ####
+head(do_levine)
+
+
+plot_do_levine <- do_levine %>% 
+  ggplot(aes(x=time2, y=do_perc)) + 
+  geom_hline(yintercept=100, linetype='dotted', col = 'blue')+
+  geom_point(size=0.5) + #geom_path()+
+  scale_x_datetime(breaks=date_breaks(width="7 days"), 
+                   labels=date_format("%m/%d", tz="America/new_york"))+ theme_bw() +
+  theme(text = element_text(size=12), axis.text.x = element_text(angle=-40))+ 
+  xlab("Time") + ylab("DO percent") #+ ylim(c(90,160))
+plot_do_levine
+
+
+
+##*Mt Pleasant SE ####
+head(do_mtpleasantse)
+
+
+plot_do_mtpleasantse <- do_mtpleasantse %>% 
+  ggplot(aes(x=time2, y=do_perc)) + 
+  geom_hline(yintercept=100, linetype='dotted', col = 'blue')+
+  geom_point(size=0.5) + #geom_path()+
+  scale_x_datetime(breaks=date_breaks(width="7 days"), 
+                   labels=date_format("%m/%d", tz="America/new_york"))+ theme_bw() +
+  theme(text = element_text(size=12), axis.text.x = element_text(angle=-40))+ 
+  xlab("Time") + ylab("DO percent") #+ ylim(c(90,160))
+plot_do_mtpleasantse
+
+
+
+##*Shelterbelt ####
+head(do_shelterbelt)
+
+
+plot_do_shelterbelt <- do_shelterbelt %>% 
+  ggplot(aes(x=time2, y=do_perc)) + 
+  geom_hline(yintercept=100, linetype='dotted', col = 'blue')+
+  geom_point(size=0.5) + #geom_path()+
+  scale_x_datetime(breaks=date_breaks(width="7 days"), 
+                   labels=date_format("%m/%d", tz="America/new_york"))+ theme_bw() +
+  theme(text = element_text(size=12), axis.text.x = element_text(angle=-40))+ 
+  xlab("Time") + ylab("DO percent") #+ ylim(c(90,160))
+plot_do_shelterbelt
+
+
+
+
+##* White ####
+head(do_white)
+
+
+plot_do_white <- do_white %>% 
+  ggplot(aes(x=time2, y=do_perc)) + 
+  geom_hline(yintercept=100, linetype='dotted', col = 'blue')+
+  geom_point(size=0.5) + #geom_path()+
+  scale_x_datetime(breaks=date_breaks(width="7 days"), 
+                   labels=date_format("%m/%d", tz="America/new_york"))+ theme_bw() +
+  theme(text = element_text(size=12), axis.text.x = element_text(angle=-40))+ 
+  xlab("Time") + ylab("DO percent") #+ ylim(c(90,160))
+plot_do_white
+
+
