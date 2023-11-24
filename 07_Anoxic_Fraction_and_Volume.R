6a9ef80ca701eac113db75b0be9b2fbd158e1995
KathrynAlta <you@example.com>
2023-11-24 15:50:18 -0500

    DO conversion table from Kalff limnology text book

--- 07_Anoxic_Fraction_and_Volume.R
+++ 07_Anoxic_Fraction_and_Volume.R
@@ -1,3 +1,11 @@
 #########################
 # ANOXIC VOLUME AND ANOXIC FRACTION 
 #########################
+
+# 0. Set Up R Environment 
+
+# 1. Input Data 
+
+# 2. Convert compiled Eureka file from DO% to DO mg/L 
+
+# 3. Get Anoxic depth for each date (Or leave R, do thi step manually, and then imput a spreadsheet of depths)
